0131eb7b709d36657c0bb6799facf2eb6d9ade7f
Liborio Cannici <lic@crd.dk>
2011-04-09 20:03:03 +0200

    Created an executable gitan-web based on Vegas

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source :rubygems
 
 gem 'sinatra'
-
+gem 'vegas'
--- app.rb
+++ app.rb
@@ -6,41 +6,49 @@ require 'gitan'
 
 require 'gitan_config'
 
-before do
-    # Nothing to do so far
-end
+class Gitan < Sinatra::Base
+  dir = File.dirname(File.expand_path(__FILE__))
 
-helpers do
-  include Sinatra::Authorization
-end
+  set :views, "#{dir}/views"
+  set :public, "#{dir}/public"
+  set :static, true
 
-get '/' do
-    require_administrative_privileges
-    @repos = Dir.open($reporoot).to_a
-    erb :repolist
-end
+  before do
+      # Nothing to do so far
+  end
 
-get '/info/:repo' do
-    require_administrative_privileges
-    @repo = params[:repo]
-    erb :repoinfo
-end
+  helpers do
+    include Sinatra::Authorization
+  end
 
-get '/commit/:repo/:rev' do
-    require_administrative_privileges
-    @repo = params[:repo]
-    @rev = params[:rev]
-    erb :commitinfo
-end
+  get '/' do
+      require_administrative_privileges
+      @repos = Dir.open($reporoot).to_a
+      erb :repolist
+  end
 
-post '/create' do
-    require_administrative_privileges
-    throw :halt, [ 400, 'Bad Request' ] if !params[:reponame]
-    throw :halt, [ 400, 'Name Too Short' ] if params[:reponame].length < 3
-    reponame = params[:reponame].gsub(/[^A-z]/,"_")
-    path = $reporoot + "/" + reponame + ".git"
-    throw :halt, [ 400, 'Repository exists' ] if File.exists? path
-    system("mkdir #{path}; cd #{path}; git init --bare")
-    redirect '/'
-end
+  get '/info/:repo' do
+      require_administrative_privileges
+      @repo = params[:repo]
+      erb :repoinfo
+  end
 
+  get '/commit/:repo/:rev' do
+      require_administrative_privileges
+      @repo = params[:repo]
+      @rev = params[:rev]
+      erb :commitinfo
+  end
+
+  post '/create' do
+      require_administrative_privileges
+      throw :halt, [ 400, 'Bad Request' ] if !params[:reponame]
+      throw :halt, [ 400, 'Name Too Short' ] if params[:reponame].length < 3
+      reponame = params[:reponame].gsub(/[^A-z]/,"_")
+      path = $reporoot + "/" + reponame + ".git"
+      throw :halt, [ 400, 'Repository exists' ] if File.exists? path
+      system("mkdir #{path}; cd #{path}; git init --bare")
+      redirect '/'
+  end
+
+end
